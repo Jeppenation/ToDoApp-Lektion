b0c4686a46b6100f8cc48e1c967ba851102930e0
jesper kallioniemi <hwilajesper@live.com>
2023-12-30 21:27:26 +0100

    done with first video

--- ToDoApp/MainWindow.xaml.cs
+++ ToDoApp/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Collections;
+using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,28 @@ namespace ToDoApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<string> _items = [];
+
         public MainWindow()
         {
             InitializeComponent();
+            Input_Name.Focus();
+
+            List_NameList.ItemsSource = _items;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(Input_Name.Text))
+            {
+                _items.Add(Input_Name.Text);
+                Input_Name.Text = string.Empty;
+            }
+            else 
+            {                 
+                MessageBox.Show("Please enter a name.");
+            }
+            
         }
     }
 }
